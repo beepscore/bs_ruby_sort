680ff5c48809ce2602dca13eb1d74fe678a5b4d7
beepscore <support@beepscore.com>
2012-04-01 05:29:29 -0700

    In array_sort.rb add comments with pseudocode solution approach.

--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -26,3 +26,26 @@ class Array
   end
 
 end
+    # algorithm
+    #
+    # Divide: Divide the n-element sequence to be sorted into two subsequences of n/2 elements each.
+    #Conquer: Sort the two subsequences recursively using Merge Sort.
+    #Combine: Merge the two sorted subsequences to produce the sorted answer.
+    #http://www.catonmat.net/blog/mit-introduction-to-algorithms-part-two/
+    #
+    # take any two elements from unsorted array e.g. first two
+    # compare and merge them to get one sorted array of length 2
+    # continue until have n/2 sorted pairs
+
+    # take any two sorted pairs
+    # merge them to get one sorted array of length 4
+    # merge operates on sorted arrays, so it knows the first elements are always lowest
+    # continue until have n/4 sorted arrays
+
+    # take any two sorted arrays of length 4
+    # merge them to get one sorted array of length 8
+    # continue until have n/8 sorted arrays
+    #
+    # continue until have 1 sorted array
+
+    #merge_bs(self.split_left_right_bs)
