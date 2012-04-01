66c369646f40e94feb3892be83818dfdd5b7156f
beepscore <support@beepscore.com>
2012-04-01 05:32:23 -0700

    Not sure why git is showing a change. Perhaps whitespace changes or reindent.

--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -8,24 +8,6 @@ class Array
   def sort_bs_merge()
     puts 'not implemented yet'
     []
-  end
-
-  # split_left_right_bs returns a hash with a left_side array and a right_side array.
-  #if array has odd number of elements, :left_side is shorter than :right_side
-  def split_left_right_bs()
-    if 0 == self.length
-      left_side = []
-      right_side = []
-    else
-      # exclusive range
-      left_side = self[0 ... self.length()/2]
-      # inclusive range
-      right_side = self[self.length()/2 .. self.length()]
-    end
-    {:left_side => left_side, :right_side => right_side} 
-  end
-
-end
     # algorithm
     #
     # Divide: Divide the n-element sequence to be sorted into two subsequences of n/2 elements each.
@@ -49,3 +31,21 @@ end
     # continue until have 1 sorted array
 
     #merge_bs(self.split_left_right_bs)
+  end
+
+  # split_left_right_bs returns a hash with a left_side array and a right_side array.
+  #if array has odd number of elements, :left_side is shorter than :right_side
+  def split_left_right_bs()
+    if 0 == self.length
+      left_side = []
+      right_side = []
+    else
+      # exclusive range
+      left_side = self[0 ... self.length()/2]
+      # inclusive range
+      right_side = self[self.length()/2 .. self.length()]
+    end
+    {:left_side => left_side, :right_side => right_side} 
+  end
+
+end
