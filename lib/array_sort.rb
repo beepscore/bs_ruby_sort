953195a2abdb8e6e9c3223afcfad73a82ac86372
beepscore <support@beepscore.com>
2012-04-01 05:33:24 -0700

    In array_sort.rb add class method merge_bs()

--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -1,6 +1,40 @@
 #!/usr/bin/env ruby
 
+require 'pp'
+
 class Array
+
+  # each arguments is an array sorted in ascending order
+  # returns an array sorted in ascending order
+  def self.merge_bs(a, b)
+
+    if (a.length > b.length)
+      #swap so that b is the longer array
+      a, b = b, a
+    end
+
+    # prepend the smaller of the next two elements to the beginning of the merged array
+    a_index = 0
+    b_index = 0
+    merged_array = []
+    while b && (b_index < b.length) do
+      if ((a_index >= a.length) ||
+          (b[b_index] < a[a_index]))
+        # we are past the end of a, or b's current element is smaller than a's
+        element_to_merge = b[b_index]
+        b_index += 1
+      else 
+        element_to_merge = a[a_index]
+        a_index += 1
+      end
+
+      # prepend element_to_merge to beginning of merged_array
+      # push is adding to beginning not end???
+      merged_array = merged_array.push(element_to_merge)
+    end
+    pp merged_array
+    merged_array
+  end
 
   # Extend class Array by adding another sort method  
   # Could use standard Array.sort() instead,
