4982eb49e7cb8b70b63e3b844dfb15ba13e5f853
beepscore <support@beepscore.com>
2012-04-01 00:14:29 -0700

    Add array_sort.rb to extend class Array with another sort method.
    Add method sort_bs_merge(), but don't fully implement.

---
+++ lib/array_sort.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+class Array
+
+  # Extend class Array by adding another sort method  
+  # Could use standard Array.sort() instead,
+  # but do this as a learning exercise.
+  def sort_bs_merge()
+    puts 'not implemented yet'
+    []
+  end
+
+end
